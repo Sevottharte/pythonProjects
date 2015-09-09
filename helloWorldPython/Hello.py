af4f5f6a9694b2d3cd78ef7072da7ad6636e5338
sevottharte <diogofelipec@gmail.com>
2015-09-08 23:12:59 -0300

    Setting coding and import some libraries

--- helloWorldPython/Hello.py
+++ helloWorldPython/Hello.py
@@ -1,3 +1,9 @@
+# coding=utf8
+
+import sys
+
+n = sys.argv[1] #pega o segundo argumento, no caso o que vem depois do caminho do programa.
+print (n)
 print "Hello World!"
 a, b = 0, 1 #atribuição de a = 0 e b = 1
 print (a)
